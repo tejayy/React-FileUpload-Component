1f8cf423656dbbcb8a587806153b0fc52c0279b3
tejayy <tbj456@gmail.com>
2024-12-03 17:11:18 +0530

    functions for uploading file

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -1,5 +1,6 @@
 import {useRef, useState} from "react";
 import "./FileUpload.css";
+import axios from "axios";
 
 const FileUpload = () => {
   const inputRef = useRef();
@@ -18,6 +19,52 @@ const FileUpload = () => {
   const onChooseFile = () => {
     inputRef.current.click();
   };
+
+  // Function to clear slected file and reset state
+  const clearFileInput = () => {
+    inputRef.current.value = "";
+    setSelectedFile(null);
+    setProgress(0);
+    setUploadStatus("select");
+  };
+
+  // Function to handle file upload
+  const handleUpload = async () => {
+    // If upload is already done, clear and return
+    if (uploadStatus === "done") {
+      clearFileInput();
+      return;
+    }
+
+    try {
+      //  Set upload status to 'uploading'
+      setUploadStatus("uploading");
+
+      // Create FormData and append selected file
+      const formData = new FormData();
+      formData.append("file", selectedFile);
+
+      // Make a asynchronous POST request to the server for file upload
+      const response = await axios.post(
+        "http://localhost:8000/api/upload",
+        formData,
+        {
+          onUploadProgress: (progressEvent) => {
+            if (progressEvent.total) {
+              const percentCompleted = Math.round(
+                (progressEvent.loaded * 100) / progressEvent.total
+              );
+              setProgress(percentCompleted);
+            }
+          },
+        }
+      );
+      setUploadStatus("done");
+    } catch (error) {
+      setUploadStatus("select");
+    }
+  };
+
   return (
     <div>
       {/* File input element with a refrence */}
@@ -43,13 +90,13 @@ const FileUpload = () => {
             <div className="file-info">
               <div style={{flex: 1}}>
                 {/* Display file name and progress bar */}
-                <h6>File Name Here</h6>
+                <h6>{selectedFile.name}</h6>
                 <div className="progress-bg">
-                  <div className="progress" style={{width: `40%`}} />
+                  <div className="progress" style={{width: `${progress}`}} />
                 </div>
               </div>
               {/* Display Clear button or upload progress */}
-              <button onClick={() => {}}>
+              <button onClick={clearFileInput}>
                 <span className="material-symbols-outlined close-icon">
                   close
                 </span>
@@ -58,8 +105,10 @@ const FileUpload = () => {
           </div>
 
           {/* Button to finalize upload or clear selection */}
-          <button className="upload-btn" onClick={() => {}}>
-            Upload
+          <button className="upload-btn" onClick={handleUpload}>
+            {uploadStatus === "select" || uploadStatus === "uploading"
+              ? "upload"
+              : "done"}
           </button>
         </>
       )}
